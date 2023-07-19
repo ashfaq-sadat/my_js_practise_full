fe61ef60dd88c9e49811ef23f0fede677fde9d12
Md. Ashfaq Sadat <68952956+ashfaq-sadat@users.noreply.github.com>
2023-07-19 18:40:46 +0600

    added oop practice

---
+++ oop_sumit/oop.js
@@ -0,0 +1,18 @@
+class Player {
+	constructor(name, age, sex, skill) {
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+		this.skill = skill;
+	}
+}
+
+const sadat = new Player("Mashrafi", 35, "Male", "Bowler");
+
+console.log(sadat.age);
+
+
+class Score extends Player {
+    super();
+    constructor()
+}
